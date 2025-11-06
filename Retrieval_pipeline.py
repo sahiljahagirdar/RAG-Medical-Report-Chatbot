f6ac5944ab2d7ca73b53e301c624666afb357649
Sahil <54531364+sahiljahagirdar@users.noreply.github.com>
2025-11-06 20:47:08 +0530

    Updated the file with changes in the library

--- Retrieval_pipeline.py
+++ Retrieval_pipeline.py
@@ -1,7 +1,7 @@
 import sys
 from langchain_openai import ChatOpenAI, OpenAIEmbeddings
 from langchain_chroma import Chroma
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_core.messages import HumanMessage, SystemMessage
 from dotenv import load_dotenv
